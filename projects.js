45f4ef6e4d3220e75787d82fd8054ffb8f6acdc7
Manish Kumar <133526263+kumarmaniish@users.noreply.github.com>
2024-07-15 20:00:17 +0000

    yes

--- projects.js
+++ projects.js
@@ -24,3 +24,44 @@ const buttons = document.querySelectorAll('.button');
       }
     });
   });
+
+//   Project2
+
+const form=document.querySelector('form');
+
+form.addEventListener('submit',function(e){
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value);
+  console.log(height);
+  const weight = parseInt(document.querySelector('#weight').value);
+  console.log(weight);
+
+  const results=document.querySelector('#results');
+
+  if(height === '' || height<0 || isNaN(height)){
+    results.innerHTML="Please enter the valid height";
+  }
+  else if(weight === '' || weight<0 || isNaN(weight)){
+    results.innerHTML="Please enter the valid wight";
+  }
+  else{
+    const bmi=(weight/((height*height)/10000)).toFixed(2);
+
+    // function
+    function predict(bmi){
+      if(bmi<18.6){
+        return "Under Weight";
+      }
+      else if(bmi>18.6 && bmi<24.9){
+        return "Normal Range";
+      }
+      else{
+        return "OverWeight";
+      }
+    }
+    
+    results.innerHTML=`<span>${bmi} and ${predict(bmi)} </span>`;
+  }
+
+});
